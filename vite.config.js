e94130f0f0d345666092dd221c9cfc4b117c7d06
Jarno Elovirta <jarno.elovirta@almamedia.fi>
2026-07-05 14:21:03 +0300

    fix: Fix JS build

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,6 @@ export default defineConfig({
     outDir: 'assets',
     rollupOptions: {
       input: {
-        main: resolve(__dirname, '_js/main.js'),
-        cover: resolve(__dirname, '_js/cover.js'),
         mainCSS: resolve(__dirname, 'css/main.scss'),
         implementer: resolve(__dirname, 'css/implementer.scss'),
         source: resolve(__dirname, 'css/source.scss'),
@@ -22,13 +20,8 @@ export default defineConfig({
           if (/\.(css|scss)$/.test(assetInfo.name)) {
             return `css/[name].[ext]`
           }
-          if (/\.(js|ts)$/.test(assetInfo.name)) {
-            return `js/[name].[ext]`
-          }
           return `[name].[ext]`
         },
-        chunkFileNames: 'js/[name].[hash].js',
-        entryFileNames: 'js/[name].js',
       },
     },
   },
